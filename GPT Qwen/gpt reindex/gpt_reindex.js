b08830ed5a44b1e1e585c969435ab075e9876fdd
krgbltn <asopov.ct@gmail.com>
2026-02-16 11:49:04 +0300

    16/02

--- GPT Qwen/gpt reindex/gpt_reindex.js
+++ GPT Qwen/gpt reindex/gpt_reindex.js
@@ -5,44 +5,24 @@ const URL_REINDEX = new URL('/make_index', agentSettings.url_context_search_inde
 async function main() {
 	let response
 
-	if (!message.message.text =="reindex") {
-		return [agentApi.makeTextReply(`No command found. Send "reindex" to reindex.`)
-		];
+	if (!message.message.text == "reindex") {
+		return [agentApi.makeTextReply(`No command found. Send "reindex" to reindex.`)];
 	}
 	try {
-		response = await axios.post(
-			URL_REINDEX,
-			{
-				autodeploy: true,
-				chunk_size: 2000,
-				customer_id: ['umka', 'docs', 'pony'],
-				format: {
-					strategy: "last_headers",
-					n_headers: 3,
-					delimiter: " >>> ",
-					add_title: false
-				},
-				do_index_messages: false,
-				text_analyzer: "russian",
-			}
-		)
+		response = await axios.post(URL_REINDEX, {
+			autodeploy: true, chunk_size: 2000, customer_id: ['umka', 'docs', 'pony'], format: {
+				strategy: "last_headers", n_headers: 3, delimiter: " >>> ", add_title: false
+			}, do_index_messages: false, text_analyzer: "russian",
+		})
 		logger.info(response.data)
-	} catch(e) {
+	} catch (e) {
 		// Логика при ошибке запроса
 		logger.info(`Error requesting context search: ${e}.`);
 		//throw e;
-		return([
-			agentApi.makeTextReply(
-				`Error: ${JSON.stringify(e)}`
-			),
-		])
+		return ([agentApi.makeTextReply(`Error: ${JSON.stringify(e)}`),])
 	}
 
-	return([
-		agentApi.makeTextReply(
-			`Created a new index for context search: ${JSON.stringify(response.data)}`
-		),
-	])
+	return ([agentApi.makeTextReply(`Created a new index for context search: ${JSON.stringify(response.data)}`),])
 }
 
 if (message.message_type === 1) {
